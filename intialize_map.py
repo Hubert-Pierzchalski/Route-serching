403cdc23f9ee3268403fb533acaac3a497ea1c90
Hubert <250367@edu.p.lodz.pl>
2024-05-27 22:38:51 +0200

    and so the journey beggins

---
+++ intialize_map.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+y = np.zeros(50)
+x = np.zeros(50)
+for i in range(50):
+    y[i] = np.random.randint(-101,101)
+    x[i] = np.random.randint(-101,101)
